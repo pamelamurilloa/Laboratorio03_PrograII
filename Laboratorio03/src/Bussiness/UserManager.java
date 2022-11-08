99fa8f19c51f6ee9b3753aacfbb4cf4dfa851598
Pamela Murillo <pamelamurilloa@gmail.com>
2022-11-08 16:43:45 -0600

    methods adquired

--- Laboratorio03/src/Bussiness/UserManager.java
+++ Laboratorio03/src/Bussiness/UserManager.java
@@ -1,6 +1,31 @@
 package Bussiness;
 
+import Data.DBManager;
+import Objects.User;
+import java.util.ArrayList;
+
 
 public class UserManager {
     
+    DBManager dbManager = new DBManager();
+    
+    public int isNumeric(String string){
+	try {
+            return Integer.parseInt(string);
+	} catch (NumberFormatException nfe){
+            return -1;
+	}
+    }
+    
+    public boolean insertUser(User user) {
+        if (dbManager.doesUserExist( user.getID() ) == false) {
+            dbManager.insertUser(user);
+            return true;
+        }
+            return false;
+    }
+    
+    public ArrayList getUserList() {
+        return dbManager.getUserIDs();
+    }
 }
