d073bf767b1692c7b5b2d3c37d90becf78905930
Pamela Murillo <pamelamurilloa@gmail.com>
2022-11-08 20:29:55 -0600

    Start done

--- Laboratorio03/src/MainStart/MainStart.java
+++ Laboratorio03/src/MainStart/MainStart.java
@@ -1,11 +1,14 @@
 package MainStart;
 
+import UserInterfaces.StartFrame;
+
 
 public class MainStart {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        StartFrame newWindow = new StartFrame();
+        newWindow.setVisible(true);
     }
     
 }
